c456a96cdd8107dc5c919637e5e7bc4b1ecb11a4
deltamemories <maxenot123@gmail.com>
2026-07-02 14:25:27 +0200

    feat(android/udp): add send proto support

--- android/app/src/main/java/com/skymouse/skymouseclient/data/UdpClientManager.kt
+++ android/app/src/main/java/com/skymouse/skymouseclient/data/UdpClientManager.kt
@@ -44,6 +44,22 @@ class UdpClientManager {
         }
     }
 
+
+
+    suspend fun sendProto(proto: com.google.protobuf.MessageLite) = withContext(Dispatchers.IO) {
+        val s = socket
+        val address = serverAddress
+        if (s == null || address == null || _connectionState.value != UdpConnectionState.Connected) return@withContext
+
+        try {
+            val bytes = proto.toByteArray()
+            val packet = DatagramPacket(bytes, bytes.size, address, serverPort)
+            s.send(packet)
+        } catch (e: Exception) {
+            _connectionState.value = UdpConnectionState.Error(e.localizedMessage ?: "Send Failed")
+        }
+    }
+
     fun disconnect() {
         socket?.close()
         socket = null
