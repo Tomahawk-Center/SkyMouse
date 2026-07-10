85b936a0ad66a23160933732d1e7fe78cb408ec3
deltamemories <maxenot123@gmail.com>
2026-07-10 09:54:39 +0200

    feat(pc/main): implement haptic feedback events support from emulator

--- pc/cmd/app/main.go
+++ pc/cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -9,16 +10,22 @@ import (
 	"github.com/Tomahawk-Center/SkyMouse/pc/internal/emulator"
 	"github.com/Tomahawk-Center/SkyMouse/pc/internal/server/tcp"
 	"github.com/Tomahawk-Center/SkyMouse/pc/internal/server/udp"
+	"github.com/Tomahawk-Center/SkyMouse/pc/pkg/protoapi"
 )
 
 func main() {
-	emu := emulator.NewEmulator(5)
+	emuEventsCh := make(chan *protoapi.ServerEvent, 20)
+	emu := emulator.NewEmulator(5, emuEventsCh)
+
 	udpServer, err := udp.NewServer(":9999", emu)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tcpServer := tcp.NewServer(":10000", emu, udpServer.Port)
+	tcpServer, err := tcp.NewServer(":10000", emu, udpServer.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	go func() {
 		if err := tcpServer.Start(); err != nil {
@@ -29,6 +36,20 @@ func main() {
 	go func() {
 		if err := udpServer.Start(); err != nil {
 			log.Fatalf("UDP server start failed: %v\n", err)
+		}
+	}()
+
+	go func() {
+		for ev := range emuEventsCh {
+			msgToClient := &protoapi.MessageToClient{
+				Event: &protoapi.MessageToClient_ServerEvent{
+					ServerEvent: ev,
+				},
+			}
+			err := udpServer.SendProto(msgToClient)
+			if err != nil {
+				log.Printf("Error sending message to client: %v\n", err)
+			}
 		}
 	}()
 
