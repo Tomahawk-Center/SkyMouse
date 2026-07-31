60ed64c70b2f52bd9ba5c62fc2fc1bac8179b428
deltamemories <maxenot123@gmail.com>
2026-07-31 20:23:44 +0200

    fix(android): correct proto contract version that renamed by mistake

--- android/app/src/main/java/com/skymouse/skymouseclient/ui/connection/ConnectionViewModel.kt
+++ android/app/src/main/java/com/skymouse/skymouseclient/ui/connection/ConnectionViewModel.kt
@@ -42,7 +42,7 @@ class ConnectionViewModel(application: Application) : AndroidViewModel(applicati
 
     fun onConnectClicked() {
         val portInt = port.toIntOrNull() ?: return
-        val clientVersionStr = "3.0"
+        val clientVersionStr = "3.1"
 
         prefs.edit {
             putString("ip_address", ipAddress)
